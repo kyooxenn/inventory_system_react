14b359533ac53da7a4df1fc60b6831101f40b463
ZIP-Norbs <norbert@zip.ph>
2025-10-12 22:25:02 +0800

    fix bug on mobile view from app

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from "react-hot-toast";
 
-// 👇 Force dark mode globally
-document.documentElement.classList.add('dark');
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
