41dadd56bceed33fc597f63acb4481b1f7cf2e17
ZIP-Norbs <norbert@zip.ph>
2025-10-12 22:17:47 +0800

    add tailwind.config.js force dark mode class

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  darkMode: 'class', // Enables manual dark mode control
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
