676cd0ce60dd9994038be30aed46e1d6a4cae664
ZIP-Norbs <norbert@zip.ph>
2025-10-13 11:15:56 +0800

    fix auth js url mapping bug

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,13 +6,13 @@ import axios from "axios";
 const API_BASE_URL = "https://inventory-system-dair.onrender.com/api/auth";
 
 export const login = async (username, password) => {
-  const response = await axios.post(`${API_URL}/login`, { username, password });
+  const response = await axios.post(`${API_BASE_URL}/login`, { username, password });
   localStorage.setItem("token", response.data.token);
   return response.data;
 };
 
 export const register = async (username, password) => {
-  const response = await axios.post(`${API_URL}/register`, { username, password });
+  const response = await axios.post(`${API_BASE_URL}/register`, { username, password });
   return response.data;
 };
 
